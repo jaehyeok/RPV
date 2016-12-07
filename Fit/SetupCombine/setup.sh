3309fb12d5cb5b95b0a01ac1241934d2ec259ba3
Rohan Bhandari <rohan.bhandari@cern.ch>
2016-12-07 03:06:19 -0800

    Updated setup script to recommended cmssw and combine version

--- Fit/SetupCombine/setup.sh
+++ Fit/SetupCombine/setup.sh
@@ -5,21 +5,23 @@
 
 # Get CMSSW and combine tool
 export SCRAM_ARCH=slc6_amd64_gcc491
-cmsrel CMSSW_7_4_7_patch1
+cmsrel CMSSW_7_4_7
 CURRENT=$PWD
-cd CMSSW_7_4_7_patch1/src
+cd CMSSW_7_4_7/src
 cmsenv
 git clone https://github.com/cms-analysis/HiggsAnalysis-CombinedLimit.git HiggsAnalysis/CombinedLimit
 cd HiggsAnalysis/CombinedLimit
-git checkout 74x-root6
+git fetch origin
+git checkout v6.3.0
 cd ../../ 
 
-# Do some customization for RPV
-cp $CURRENT/SetupCombine/FitterAlgoBase.cc HiggsAnalysis/CombinedLimit/src/
-cp $CURRENT/SetupCombine/MaxLikelihoodFit.cc HiggsAnalysis/CombinedLimit/src/
-cp $CURRENT/SetupCombine/MultiDimFit.cc HiggsAnalysis/CombinedLimit/src/
-cp $CURRENT/SetupCombine/MultiDimFit.h HiggsAnalysis/CombinedLimit/interface/
-cp $CURRENT/SetupCombine/RPVModel.py HiggsAnalysis/CombinedLimit/python
+# Do some customization for RPV (Note: These files are for the 2015 Fit Model)
+#cp $CURRENT/SetupCombine/FitterAlgoBase.cc HiggsAnalysis/CombinedLimit/src/
+#cp $CURRENT/SetupCombine/MaxLikelihoodFit.cc HiggsAnalysis/CombinedLimit/src/
+#cp $CURRENT/SetupCombine/MultiDimFit.cc HiggsAnalysis/CombinedLimit/src/
+#cp $CURRENT/SetupCombine/MultiDimFit.h HiggsAnalysis/CombinedLimit/interface/
+#cp $CURRENT/SetupCombine/RPVModel.py HiggsAnalysis/CombinedLimit/python/
 
 # Build the beast
+scramv1 b clean
 scramv1 b -j 10

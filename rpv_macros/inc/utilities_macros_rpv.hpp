c786b159dd454769e0035684b3abdfbfd65a5755
Jae Hyeok Yoo <jaehyeokyoo@gmail.com>
2017-01-16 11:23:02 +0100

    added function(passBinCut) that tells which bin an event belongs to

--- rpv_macros/inc/utilities_macros_rpv.hpp
+++ rpv_macros/inc/utilities_macros_rpv.hpp
@@ -2,7 +2,7 @@
 
 namespace rpv
 {
-  TString luminosity="12.9";
+  TString luminosity="35";
 }
 
 TString filestring(TString dataset, bool isSkimmed=true)
@@ -44,6 +44,11 @@ std::vector<TString> getRPVProcess(TString folder, TString process){
     else if(process=="rpv_m1300") files.push_back(folder+"*mGluino*1300*");
     else if(process=="rpv_m1400") files.push_back(folder+"*mGluino*1400*");
     else if(process=="rpv_m1500") files.push_back(folder+"*mGluino*1500*");
+    else if(process=="rpv_m1600") files.push_back(folder+"*mGluino*1600*");
+    else if(process=="rpv_m1700") files.push_back(folder+"*mGluino*1700*");
+    else if(process=="rpv_m1800") files.push_back(folder+"*mGluino*1800*");
+    else if(process=="rpv_m1900") files.push_back(folder+"*mGluino*1900*");
+    else if(process=="rpv_m2000") files.push_back(folder+"*mGluino*2000*");
   }
   // For 0, 1, or 2 lepton ttbar apply a ntruleps cut at the sfeat level
   else if(process=="ttbar"){
@@ -123,3 +128,269 @@ std::vector<TString> getRPVProcess(TString folder, TString process){
   }
   return files;
 }
+
+
+//
+//
+//
+bool passBinCut(int bin, int nleps_, float ht_, int njets_, float mj_)
+{
+    bool pass = false;
+    
+    //
+    int     nleps     = 99;
+    float   ht        = 999999;
+    int     njetsLow  = 99;
+    int     njetsHigh = 99;
+    float   mjLow     = 999999;
+    float   mjHigh    = 999999;
+    
+    // cut values
+    const float ht0lepCut    = 1500;
+    const float ht1lepCut    = 1200;
+
+    const float mjVLowCut    = 300;
+    const float mjLowCut     = 500;
+    const float mjMedCut     = 800;
+    const float mjHighCut    = 1000;
+    const float mjInfCut     = 9999999;
+    
+    const float njetsVLowCut = 4;
+    const float njetsLowCut  = 6;
+    const float njetsMedCut  = 8;
+    const float njetsHighCut = 10; 
+    const float njetsInfCut  = 999; 
+
+    // Get cut values for a given bin
+    if(bin==0) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsVLowCut;
+        njetsHigh   = njetsVLowCut+1;
+        mjLow       = mjLowCut;
+        mjHigh      = mjMedCut;
+    } 
+    else if(bin==1) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsLowCut;
+        njetsHigh   = njetsLowCut+1;
+        mjLow       = mjLowCut;
+        mjHigh      = mjMedCut;
+    }
+    else if(bin==2) 
+    { 
+        nleps       = 1;
+        ht          = ht1lepCut;
+        njetsLow    = njetsVLowCut;
+        njetsHigh   = njetsVLowCut+1;
+        mjLow       = mjLowCut;
+        mjHigh      = mjMedCut;
+    }
+    else if(bin==3) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsVLowCut;
+        njetsHigh   = njetsVLowCut+1;
+        mjLow       = mjMedCut;
+        mjHigh      = mjInfCut;
+    } 
+    else if(bin==4) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsLowCut;
+        njetsHigh   = njetsLowCut+1;
+        mjLow       = mjMedCut;
+        mjHigh      = mjInfCut;
+    }
+    else if(bin==5) 
+    { 
+        nleps       = 1;
+        ht          = ht1lepCut;
+        njetsLow    = njetsVLowCut;
+        njetsHigh   = njetsVLowCut+1;
+        mjLow       = mjMedCut;
+        mjHigh      = mjInfCut;
+    }
+    else if(bin==6) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsVLowCut;
+        njetsHigh   = njetsVLowCut+1;
+        mjLow       = mjVLowCut;
+        mjHigh      = mjLowCut;
+    }
+    else if(bin==7) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsLowCut;
+        njetsHigh   = njetsLowCut+1;
+        mjLow       = mjVLowCut;
+        mjHigh      = mjLowCut;
+    }
+    else if(bin==8) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsMedCut;
+        njetsHigh   = njetsMedCut+1;
+        mjLow       = mjVLowCut;
+        mjHigh      = mjLowCut;
+    }
+    else if(bin==9) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsHighCut;
+        njetsHigh   = njetsInfCut;
+        mjLow       = mjVLowCut;
+        mjHigh      = mjLowCut;
+    }
+    else if(bin==10) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsHighCut;
+        njetsHigh   = njetsInfCut;
+        mjLow       = mjLowCut;
+        mjHigh      = mjMedCut;
+    }
+    else if(bin==11) 
+    { 
+        nleps       = 1;
+        ht          = ht1lepCut;
+        njetsLow    = njetsLowCut;
+        njetsHigh   = njetsLowCut+1;
+        mjLow       = mjLowCut;
+        mjHigh      = mjMedCut;
+    }
+    else if(bin==12) 
+    { 
+        nleps       = 1;
+        ht          = ht1lepCut;
+        njetsLow    = njetsMedCut;
+        njetsHigh   = njetsInfCut;
+        mjLow       = mjLowCut;
+        mjHigh      = mjMedCut;
+    }
+    else if(bin==13) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsHighCut;
+        njetsHigh   = njetsInfCut;
+        mjLow       = mjMedCut;
+        mjHigh      = mjHighCut;
+    }
+    else if(bin==14) 
+    { 
+        nleps       = 1;
+        ht          = ht1lepCut;
+        njetsLow    = njetsLowCut;
+        njetsHigh   = njetsLowCut+1;
+        mjLow       = mjMedCut;
+        mjHigh      = mjHighCut;
+    }
+    else if(bin==15) 
+    { 
+        nleps       = 1;
+        ht          = ht1lepCut;
+        njetsLow    = njetsMedCut;
+        njetsHigh   = njetsInfCut;
+        mjLow       = mjMedCut;
+        mjHigh      = mjHighCut;
+    }
+    else if(bin==16) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsMedCut;
+        njetsHigh   = njetsMedCut+1;
+        mjLow       = mjLowCut;
+        mjHigh      = mjMedCut;
+    }
+    else if(bin==17) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsMedCut;
+        njetsHigh   = njetsMedCut+1;
+        mjLow       = mjMedCut;
+        mjHigh      = mjHighCut;
+    }
+    else if(bin==18) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsMedCut;
+        njetsHigh   = njetsMedCut+1;
+        mjLow       = mjHighCut;
+        mjHigh      = mjInfCut;
+    }
+    else if(bin==19) 
+    { 
+        nleps       = 0;
+        ht          = ht0lepCut;
+        njetsLow    = njetsHighCut;
+        njetsHigh   = njetsInfCut;
+        mjLow       = mjHighCut;
+        mjHigh      = mjInfCut;
+    }
+    else if(bin==20) 
+    { 
+        nleps       = 1;
+        ht          = ht1lepCut;
+        njetsLow    = njetsLowCut;
+        njetsHigh   = njetsLowCut+1;
+        mjLow       = mjHighCut;
+        mjHigh      = mjInfCut;
+    }
+    else if(bin==21) 
+    { 
+        nleps       = 1;
+        ht          = ht1lepCut;
+        njetsLow    = njetsMedCut;
+        njetsHigh   = njetsInfCut;
+        mjLow       = mjHighCut;
+        mjHigh      = mjInfCut;
+    }
+    else  // in case of wrong bin number
+    { 
+        std::cout << "[Error] I don't like your bin number: " << bin << " !!" << std::endl; 
+        nleps       = 99;
+        ht          = 9999999;
+        njetsLow    = njetsInfCut;
+        njetsHigh   = njetsInfCut;
+        mjLow       = mjInfCut;
+        mjHigh      = mjInfCut;
+    }
+
+    if(0)  // debugging 
+    { 
+       std::cout << "[Debug] bin " << bin << " :: " 
+                 << "htCut    : " << ht << ", "  
+                 << "njetsCut : " << njetsLow << "-" << njetsHigh << ", " 
+                 << "mjCut    : " << mjLow << "-" << mjHigh << " " 
+                 << std::endl;
+    }
+
+    //
+    // Apply cuts
+    //
+    if(nleps_==nleps 
+       && ht_>ht
+       && njets_>=njetsLow
+       && njets_<=njetsHigh
+       && mj_>mjLow
+       && mj_<=mjHigh
+       ) pass = true;
+
+    return pass;
+}
+

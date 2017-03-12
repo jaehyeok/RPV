20bd4fefc06ce771353b3fb42c6a838474202109
Rohan Bhandari <rohan@physics.ucsb.edu>
2017-03-12 18:41:25 +0100

    Added ratio plot and other formatting to plot_csvfit

--- Fit/plot_csvfit.py
+++ Fit/plot_csvfit.py
@@ -1,6 +1,7 @@
 import argparse
 import math
 from sys import argv
+import os.path
 
 #Batch mode hack
 argv.append( '-b-' )
@@ -8,8 +9,39 @@ import ROOT
 ROOT.gROOT.SetBatch(True)
 argv.remove( '-b-' )
 
-def addInQuad(x, y):
-    return math.sqrt(pow(x,2)+pow(y,2))
+def getFlavorWeights(prefit, postfit):
+    processes = ['other','qcd_b','qcd_c','qcd_l']
+
+    prefit_qcdb = [prefit.find('bin1/qcd_b').getVal(), 0]
+    prefit_qcdc = [prefit.find('bin1/qcd_c').getVal(), 0]
+    postfit_qcdb = [postfit.find('bin1/qcd_b').getVal(), postfit.find('bin1/qcd_b').getError()]
+    postfit_qcdc = [postfit.find('bin1/qcd_c').getVal(), postfit.find('bin1/qcd_c').getError()]
+
+    prefit_norm, postfit_norm = 0, 0
+    for iproc in processes:
+        prefit_norm += prefit.find('bin1/'+iproc).getVal()
+        postfit_norm += postfit.find('bin1/'+iproc).getVal()
+
+    # Calculate weights and errors [qcdb/qcdc][weight/error]
+    weights = [[(postfit_qcdb[0]/postfit_norm)/(prefit_qcdb[0]/prefit_norm), (postfit_qcdb[1]/postfit_norm)/(prefit_qcdb[0]/prefit_norm)]]
+    weights.append([(postfit_qcdc[0]/postfit_norm)/(prefit_qcdc[0]/prefit_norm), (postfit_qcdc[1]/postfit_norm)/(prefit_qcdc[0]/prefit_norm)])
+
+    return weights
+
+def getTitleStrings(filename):
+    filename = os.path.basename(filename)
+
+    titles = ''
+    if 'vlowmj' in filename: titles = ['csvfit_postfit_vlowmj.pdf','N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, 300 #leq M_{J} #leq 500']
+    elif 'lowmj' in filename:  titles = ['csvfit_postfit_lowmj.pdf', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, 500 #leq M_{J} #leq 800']
+    elif 'highmj' in filename: titles = ['csvfit_postfit_highmj.pdf','N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, M_{J} #geq 800']
+    elif '45jets' in filename: titles = ['csvfit_postfit_45jets.pdf', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 5, M_{J} #geq 500']
+    elif '67jets' in filename: titles = ['csvfit_postfit_67jets.pdf', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 6 #leq N_{jets}#leq 7, M_{J} #geq 500']
+    elif '89jets' in filename: titles = ['csvfit_postfit_89jets.pdf', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 8 #leq N_{jets}#leq 9, M_{J} #geq 500']
+    elif '10jets' in filename: titles = ['csvfit_postfit_10jets.pdf', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, N_{jets}#geq 10, M_{J} #geq 500']
+    else:                      titles = ['csvfit_postfit.pdf', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, M_{J} #geq 500']
+
+    return titles
 
 if __name__ == '__main__':
     
@@ -25,6 +57,7 @@ if __name__ == '__main__':
     h_data = f_shapes.Get('bin1/data_obs')
 
     f_mlfit = ROOT.TFile(args.mlfit,'READ')
+    # Histograms have non-physical binning
     h_qcdb_raw = f_mlfit.Get('shapes_fit_b/bin1/qcd_b')
     h_qcdc_raw = f_mlfit.Get('shapes_fit_b/bin1/qcd_c')
     h_qcdl_raw = f_mlfit.Get('shapes_fit_b/bin1/qcd_l')
@@ -50,10 +83,22 @@ if __name__ == '__main__':
         h_other.SetBinContent(i,h_other_raw.GetBinContent(i))
         h_other.SetBinError(i,h_other_raw.GetBinError(i))
 
-    # Make the plot
-    c = ROOT.TCanvas()
+    # Get flavor weights and errors
+    weights = getFlavorWeights(f_mlfit.Get("norm_prefit"), f_mlfit.Get("norm_fit_b"))
+
+    ########################################
+    #          Plot-Making                 #
+    ########################################
 
     ROOT.gStyle.SetOptStat(0)
+    c = ROOT.TCanvas('c','c',600,600)
+    pad = ROOT.TPad('pad','pad', 0.0, 0.20, 1.0, 1.0)    
+    pad.SetTopMargin(0.10)
+    pad.SetBottomMargin(0.04)
+    pad.SetLeftMargin(0.125)
+    pad.SetRightMargin(0.04)
+    pad.Draw()
+    pad.cd()
 
     # "Stack" histograms
     h_qcdb.Add(h_qcdc)
@@ -65,17 +110,20 @@ if __name__ == '__main__':
     
     h_qcdl.Add(h_other)
 
-    h_qcdb.SetMinimum(0)
-    h_qcdb.SetTitle('Flavor Fit')
-
-
-    #Make pretty
+    # Make pretty
     h_data.SetMarkerStyle(20)
     h_data.SetMarkerColor(ROOT.kBlack)
     h_data.SetLineColor(ROOT.kBlack)
 
+    h_qcdb.SetMinimum(0)
     h_qcdb.SetLineColor(ROOT.kBlack)
     h_qcdb.SetFillColor(ROOT.kCyan+3)
+    h_qcdb.SetTitle(getTitleStrings(args.shapes)[1])
+    h_qcdb.GetXaxis().SetLabelSize(0)
+    h_qcdb.GetYaxis().SetTitle('Entries')
+    h_qcdb.GetYaxis().SetTitleSize(0.05)
+    h_qcdb.GetYaxis().SetTitleOffset(1.3)
+    h_qcdb.GetYaxis().SetLabelSize(0.04)
     h_qcdc.SetLineColor(ROOT.kBlack)
     h_qcdc.SetFillColor(ROOT.kRed+3)
     h_qcdl.SetLineColor(ROOT.kBlack)
@@ -83,11 +131,9 @@ if __name__ == '__main__':
     h_other.SetLineColor(ROOT.kBlack)
     h_other.SetFillColor(ROOT.kOrange-6)
 
-
-
-    #Draw plots now
-    h_qcdb.Draw("hist")
-    #Plot again with foramtting for error band
+    # Draw plots
+    h_qcdb.DrawCopy("hist")
+    # Plot again with formatting for error band
     h_qcdb.SetFillColor(ROOT.kBlack)
     h_qcdb.SetFillStyle(3002)
     h_qcdb.DrawCopy("E2 same")
@@ -99,15 +145,44 @@ if __name__ == '__main__':
     h_other.Draw("hist same")
     h_data.Draw('P0 same')
 
-    #Draw legend
-    leg = ROOT.TLegend(0.15,0.6,0.48,0.85)
+    # Draw legend
+    ROOT.gStyle.SetLegendTextSize(0.038)
+    leg = ROOT.TLegend(0.15,0.55,0.60,0.9)
     leg.SetFillStyle(0)
-    leg.SetBorderSize(0)
+    leg.SetBorderSize(0)    
     leg.AddEntry(h_data, 'Data')
-    leg.AddEntry(h_qcdb, 'b-quark events', 'f')
-    leg.AddEntry(h_qcdc, 'c-quark events', 'f')
-    leg.AddEntry(h_qcdl, 'l-quark events', 'f')
-    leg.AddEntry(h_other, 'Non-QCD events', 'f')
+    leg.AddEntry(h_qcdb, 'b-quark events (w_{b} = '+str(round(weights[0][0],3))+' #pm '+str(round(weights[0][1],3))+')', 'f')
+    leg.AddEntry(h_qcdc, 'c-quark events (w_{c} = '+str(round(weights[1][0],3))+' #pm '+str(round(weights[1][1],3))+')', 'f')
+    leg.AddEntry(h_qcdl, 'light-quark events (Fixed)', 'f')
+    leg.AddEntry(h_other, 'Non-QCD events (Fixed)', 'f')
     leg.Draw()
 
-    c.SaveAs('csvfit_postfit.png')
+    # Make ratio pad
+    c.cd()
+    bpad = ROOT.TPad('bpad','bpad', 0.0, 0.0, 1.0, 0.22)
+    bpad.Draw()
+    bpad.cd()
+    bpad.SetTopMargin(0.08)
+    bpad.SetBottomMargin(0.3)
+    bpad.SetRightMargin(0.04)
+    bpad.SetLeftMargin(0.125)
+    bpad.SetGridy()
+
+    h_ratio = h_data.Clone('h_ratio')
+    h_ratio.Divide(h_qcdb)
+
+    h_ratio.SetMinimum(0.90)
+    h_ratio.SetMaximum(1.10)
+    h_ratio.SetTitle('')
+    h_ratio.SetTitleSize(0.15,'XY')
+    h_ratio.GetXaxis().SetTitle('CSV')   
+    h_ratio.GetXaxis().SetTitleOffset(0.9)
+    h_ratio.GetYaxis().SetTitle('Data/Fit')
+    h_ratio.GetYaxis().CenterTitle()
+    h_ratio.GetYaxis().SetTitleOffset(0.4)
+    h_ratio.GetYaxis().SetNdivisions(3,5,0)
+    h_ratio.SetLabelSize(0.15,'XY')
+
+    h_ratio.Draw('e0')
+
+    c.SaveAs(getTitleStrings(args.shapes)[0])

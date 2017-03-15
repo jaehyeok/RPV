05ba1c642e0ada4e9427fc8002f6cce44325781f
Rohan Bhandari <rohan@physics.ucsb.edu>
2017-03-15 15:32:55 +0100

    Added post-fit pulls plot to plot_csvfit.py

--- Fit/plot_csvfit.py
+++ Fit/plot_csvfit.py
@@ -10,74 +10,9 @@ import ROOT
 ROOT.gROOT.SetBatch(True)
 argv.remove( '-b-' )
 
-def formatHist(hist, markStyle=-1, markColor=-1, lineColor=-1, fillColor=-1, fillStyle=-1):
-    if markStyle!=-1: hist.SetMarkerStyle(markStyle)
-    if markColor!=-1: hist.SetMarkerColor(markColor)
-    if lineColor!=-1: hist.SetLineColor(lineColor)
-    if fillColor!=-1: hist.SetFillColor(fillColor)
-    if fillStyle!=-1: hist.SetFillStyle(fillStyle)
-
-def getErrHists(postfit, h_qcdb, h_qcdc, h_qcdl, h_other):
-    scale_qcdb = postfit.find('bin1/qcd_b').getError()/postfit.find('bin1/qcd_b').getVal()
-    scale_qcdc = postfit.find('bin1/qcd_c').getError()/postfit.find('bin1/qcd_c').getVal()
-
-    # Anti-correlate qcdb and qcdc uncertanties
-    err = [abs(h_qcdb.GetBinContent(i)*scale_qcdb - h_qcdc.GetBinContent(i)*scale_qcdc) for i in range(1,nBins+1)] 
-
-    h_err = h_qcdb.Clone('h_err')
-    h_err.Add(h_qcdc); h_err.Add(h_qcdl); h_err.Add(h_other)
-    h_err_rat = h_err.Clone('h_err_rat')
-
-    # Add errors and make ratio band
-    for i in range(1,nBins+1): h_err.SetBinError(i,err[i-1])
-    h_err_rat.Divide(h_err)
-
-    return h_err, h_err_rat
-
-def getFlavorWeights(prefit, postfit):
-    processes = ['other','qcd_b','qcd_c','qcd_l']
-
-    prefit_qcdb = [prefit.find('bin1/qcd_b').getVal(), 0]
-    prefit_qcdc = [prefit.find('bin1/qcd_c').getVal(), 0]
-    postfit_qcdb = [postfit.find('bin1/qcd_b').getVal(), postfit.find('bin1/qcd_b').getError()]
-    postfit_qcdc = [postfit.find('bin1/qcd_c').getVal(), postfit.find('bin1/qcd_c').getError()]
-
-    prefit_norm, postfit_norm = 0, 0
-    for iproc in processes:
-        prefit_norm += prefit.find('bin1/'+iproc).getVal()
-        postfit_norm += postfit.find('bin1/'+iproc).getVal()
-
-    # Calculate weights and errors [qcdb/qcdc][weight/error]
-    weights = [[(postfit_qcdb[0]/postfit_norm)/(prefit_qcdb[0]/prefit_norm), (postfit_qcdb[1]/postfit_norm)/(prefit_qcdb[0]/prefit_norm)], 
-               [(postfit_qcdc[0]/postfit_norm)/(prefit_qcdc[0]/prefit_norm), (postfit_qcdc[1]/postfit_norm)/(prefit_qcdc[0]/prefit_norm)]]
-
-    return weights
-
-def getTitleStrings(filename):
-    filename = os.path.basename(filename)
-
-    titles = ''
-    if 'vlowmj' in filename: titles =   ['csvfit_postfit_vlowmj.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, 300 #leq M_{J} #leq 500']
-    elif 'lowmj' in filename:  titles = ['csvfit_postfit_lowmj.png',  'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, 500 #leq M_{J} #leq 800']
-    elif 'highmj' in filename: titles = ['csvfit_postfit_highmj.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, M_{J} #geq 800']
-    elif '45jets' in filename: titles = ['csvfit_postfit_45jets.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 5, M_{J} #geq 500']
-    elif '67jets' in filename: titles = ['csvfit_postfit_67jets.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 6 #leq N_{jets}#leq 7, M_{J} #geq 500']
-    elif '89jets' in filename: titles = ['csvfit_postfit_89jets.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 8 #leq N_{jets}#leq 9, M_{J} #geq 500']
-    elif '10jets' in filename: titles = ['csvfit_postfit_10jets.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, N_{jets}#geq 10, M_{J} #geq 500']
-    else:                      titles = ['csvfit_postfit.png',        'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, M_{J} #geq 500']
-
-    return titles
-
-if __name__ == '__main__':
-    
-    #####GLOBAL VALUES#####
-    nBins = 20
-    xMin = 0.8484
-    xMax = 1
-    #######################
-
+def main():
     parser = argparse.ArgumentParser(description='Plot the post-fit shapes from the CSV Fit')
-    parser.add_argument('mlfit', help='mlfit.root file with the post-fit shapes')
+    parser.add_argument('mlfit', help='mlfit file with the post-fit shapes')
     parser.add_argument('shapes', help='Pre-fit shapes file in order to get data shape')
     args = parser.parse_args()
 
@@ -160,8 +95,7 @@ if __name__ == '__main__':
     # Draw legend
     ROOT.gStyle.SetLegendTextSize(0.038)
     leg = ROOT.TLegend(0.15,0.55,0.60,0.9)
-    leg.SetFillStyle(0)
-    leg.SetBorderSize(0)    
+    leg.SetFillStyle(0); leg.SetBorderSize(0)    
     leg.AddEntry(h_data, 'Data')
     leg.AddEntry(h_qcdb, 'b-quark events (w_{b} = '+str(round(weights[0][0],3))+' #pm '+str(round(weights[0][1],3))+')', 'f')
     leg.AddEntry(h_qcdc, 'c-quark events (w_{c} = '+str(round(weights[1][0],3))+' #pm '+str(round(weights[1][1],3))+')', 'f')
@@ -197,4 +131,132 @@ if __name__ == '__main__':
     h_err_rat.Draw('E2')
     h_ratio.Draw('E0 same')
 
-    c.SaveAs(getTitleStrings(args.shapes)[0])
+    c.SaveAs(getTitleStrings(args.shapes)[0])    
+
+    makePullsPlot(f_mlfit.Get('fit_b'), getTitleStrings(args.shapes))
+
+def formatHist(hist, markStyle=-1, markSize=-1, markColor=-1, lineColor=-1, lineWidth=-1, fillColor=-1, fillStyle=-1):
+    if markStyle!=-1: hist.SetMarkerStyle(markStyle)
+    if markSize !=-1: hist.SetMarkerSize(markSize)
+    if markColor!=-1: hist.SetMarkerColor(markColor)
+    if lineColor!=-1: hist.SetLineColor(lineColor)
+    if lineWidth!=-1: hist.SetLineWidth(lineWidth)
+    if fillColor!=-1: hist.SetFillColor(fillColor)
+    if fillStyle!=-1: hist.SetFillStyle(fillStyle)
+
+def getErrHists(postfit, h_qcdb, h_qcdc, h_qcdl, h_other):
+    scale_qcdb = postfit.find('bin1/qcd_b').getError()/postfit.find('bin1/qcd_b').getVal()
+    scale_qcdc = postfit.find('bin1/qcd_c').getError()/postfit.find('bin1/qcd_c').getVal()
+
+    # Anti-correlate qcdb and qcdc uncertanties
+    err = [abs(h_qcdb.GetBinContent(i)*scale_qcdb - h_qcdc.GetBinContent(i)*scale_qcdc) for i in range(1,nBins+1)] 
+
+    h_err = h_qcdb.Clone('h_err')
+    h_err.Add(h_qcdc); h_err.Add(h_qcdl); h_err.Add(h_other)
+    h_err_rat = h_err.Clone('h_err_rat')
+
+    # Add errors and make ratio band
+    for i in range(1,nBins+1): h_err.SetBinError(i,err[i-1])
+    h_err_rat.Divide(h_err)
+
+    return h_err, h_err_rat
+
+def getFlavorWeights(prefit, postfit):
+    processes = ['other','qcd_b','qcd_c','qcd_l']
+
+    prefit_qcdb = [prefit.find('bin1/qcd_b').getVal(), 0]
+    prefit_qcdc = [prefit.find('bin1/qcd_c').getVal(), 0]
+    postfit_qcdb = [postfit.find('bin1/qcd_b').getVal(), postfit.find('bin1/qcd_b').getError()]
+    postfit_qcdc = [postfit.find('bin1/qcd_c').getVal(), postfit.find('bin1/qcd_c').getError()]
+
+    prefit_norm, postfit_norm = 0, 0
+    for iproc in processes:
+        prefit_norm += prefit.find('bin1/'+iproc).getVal()
+        postfit_norm += postfit.find('bin1/'+iproc).getVal()
+
+    # Calculate weights and errors [qcdb/qcdc][weight/error]
+    weights = [[(postfit_qcdb[0]/postfit_norm)/(prefit_qcdb[0]/prefit_norm), (postfit_qcdb[1]/postfit_norm)/(prefit_qcdb[0]/prefit_norm)], 
+               [(postfit_qcdc[0]/postfit_norm)/(prefit_qcdc[0]/prefit_norm), (postfit_qcdc[1]/postfit_norm)/(prefit_qcdc[0]/prefit_norm)]]
+
+    return weights
+
+def getTitleStrings(filename):
+    filename = os.path.basename(filename)
+
+    titles = ''
+    if 'vlowmj' in filename: titles =   ['csvfit_postfit_vlowmj.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, 300 #leq M_{J} #leq 500']
+    elif 'lowmj' in filename:  titles = ['csvfit_postfit_lowmj.png',  'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, 500 #leq M_{J} #leq 800']
+    elif 'highmj' in filename: titles = ['csvfit_postfit_highmj.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, M_{J} #geq 800']
+    elif '45jets' in filename: titles = ['csvfit_postfit_45jets.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 5, M_{J} #geq 500']
+    elif '67jets' in filename: titles = ['csvfit_postfit_67jets.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 6 #leq N_{jets}#leq 7, M_{J} #geq 500']
+    elif '89jets' in filename: titles = ['csvfit_postfit_89jets.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 8 #leq N_{jets}#leq 9, M_{J} #geq 500']
+    elif '10jets' in filename: titles = ['csvfit_postfit_10jets.png', 'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, N_{jets}#geq 10, M_{J} #geq 500']
+    else:                      titles = ['csvfit_postfit.png',        'N_{leps} = 0, H_{T} > 1500, N_{b} #geq 2, 4 #leq N_{jets}#leq 7, M_{J} #geq 500']
+
+    return titles
+
+def makePullsPlot(fitb, titles):
+    params = fitb.floatParsFinal()
+    npulls = params.getSize()
+
+    #Make histogram
+    h_pulls = ROOT.TH1D('h_pulls',';;Post-fit pulls',npulls,0,npulls)
+    formatHist(h_pulls,markStyle=20, markSize=0.6, lineColor=ROOT.kBlack, lineWidth=1)
+    for i in range(npulls):
+        param = params.at(i)
+        h_pulls.SetBinContent(i+1,param.getVal())
+        h_pulls.SetBinError(i+1,param.getError())
+        h_pulls.GetXaxis().SetBinLabel(i+1,param.GetName())
+
+    h_pulls.SetTitleSize(0.045,'Y'); h_pulls.SetTitleOffset(0.62,'Y')
+    h_pulls.SetMinimum(-3); h_pulls.SetMaximum(3)
+
+    # Color code nuisances
+    h_qcdb, h_qcdc, h_qcdl, h_other = h_pulls.Clone('h_qcdb'), h_pulls.Clone('h_qcdc'), h_pulls.Clone('h_qcdl'), h_pulls.Clone('h_other')
+    formatHist(h_qcdb,lineColor=ROOT.kCyan+3,markColor=ROOT.kCyan+3)
+    formatHist(h_qcdc,lineColor=ROOT.kRed+3,markColor=ROOT.kRed+3)
+    formatHist(h_qcdl,lineColor=ROOT.kViolet+3,markColor=ROOT.kViolet+3)
+    formatHist(h_other,lineColor=ROOT.kOrange-6,markColor=ROOT.kOrange-6)
+    for i in range(npulls):
+        # Set contents off the plot if not correct nuisance
+        if 'qcd_b' not in h_qcdb.GetXaxis().GetBinLabel(i): h_qcdb.SetBinContent(i,50)
+        if 'qcd_c' not in h_qcdc.GetXaxis().GetBinLabel(i): h_qcdc.SetBinContent(i,50)
+        if 'qcd_l' not in h_qcdl.GetXaxis().GetBinLabel(i): h_qcdl.SetBinContent(i,50)
+        if 'other' not in h_other.GetXaxis().GetBinLabel(i): h_other.SetBinContent(i,50)
+
+    #Make box
+    box = ROOT.TBox(0, -1, npulls, 1)
+    formatHist(box, fillStyle=3003, fillColor=ROOT.kBlue)
+
+    #Draw Plot
+    cpulls = ROOT.TCanvas('cpulls','cpulls',800,600)
+    cpulls.SetBottomMargin(0.22); 
+    cpulls.SetLeftMargin(0.07); cpulls.SetRightMargin(0.02)
+
+    h_pulls.Draw('EX0P')
+    box.Draw()
+    h_other.Draw('EX0P same')
+    h_qcdb.Draw('EX0P same')
+    h_qcdc.Draw('EX0P same')
+    h_qcdl.Draw('EX0P same')
+
+    #Add legend
+    ROOT.gStyle.SetLegendTextSize(0.038)
+    leg = ROOT.TLegend(0.06,0.90,0.95,1.00)
+    leg.SetFillStyle(0); leg.SetBorderSize(0); leg.SetNColumns(4)
+    leg.AddEntry(h_qcdb, 'b-quark events', 'pe')
+    leg.AddEntry(h_qcdc, 'c-quark events', 'pe')
+    leg.AddEntry(h_qcdl, 'light-quark events', 'pe')
+    leg.AddEntry(h_other, 'Non-QCD events', 'pe')
+    leg.Draw()
+
+    cpulls.SaveAs(titles[0].replace('postfit','pulls'))
+
+if __name__ == '__main__':
+    #####GLOBAL VALUES#####
+    nBins = 20
+    xMin = 0.8484
+    xMax = 1
+    #######################
+
+    main()

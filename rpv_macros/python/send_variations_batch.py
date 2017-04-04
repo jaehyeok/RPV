257e6fe94ee666f774511dc9424724a6be4c0d34
Rohan Bhandari <rohan.bhandari@cern.ch>
2017-04-04 02:52:45 -0700

    Added script to send variations to the batch instead of condor

---
+++ rpv_macros/python/send_variations_batch.py
@@ -0,0 +1,18 @@
+#!/bin/env python
+import os
+
+VARLIST = ['nominal','btag_bc', 'btag_udsg',
+           'gs',
+           'jes', 'jer',
+           'lep_eff', 'ttbar_pt', #'pileup',
+           'isr',
+           'qcd_flavor',
+           'qcd_mur', 'qcd_muf', 'qcd_murf',
+           'ttbar_mur', 'ttbar_muf', 'ttbar_murf',
+           'wjets_mur', 'wjets_muf', 'wjets_murf',
+           'other_mur', 'other_muf', 'other_murf',
+           'signal_mur', 'signal_muf', 'signal_murf']
+
+
+for var in VARLIST:
+    os.system('JobSubmit.csh ./varscripts/wrapper.sh ./run/make_variations.exe ' + var)

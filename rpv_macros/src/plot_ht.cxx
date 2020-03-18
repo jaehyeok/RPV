fe72b04985e56d2cd563ac79994e45f61a7a3ddd
YongHo, Jeong <yonghojeong57@gmail.com>
2020-03-18 18:26:27 +0900

    update for drawing Ht plot
    include all HT region.

--- rpv_macros/src/plot_ht.cxx
+++ rpv_macros/src/plot_ht.cxx
@@ -7,19 +7,19 @@
 #include <iomanip>
 
 #include "TCanvas.h"
-#include "TH1.h"
+#include "TH1F.h"
 #include "TFile.h"
 #include "TStyle.h"
 #include "TLegend.h"
 #include "TDirectory.h"
 #include "TString.h"
 #include "TTree.h"
-
+#include "THStack.h"
 using namespace std;
 
-void set_histo_style(TH1F *h1){
-	h1->GetXaxis()->SetTitle("HT");
-	h1->SetLineWidth(1.2);
+void set_histo_style(TH1F *h){
+	h->GetXaxis()->SetTitle("HT");
+	h->SetLineWidth(1.2);
 }
 
 void set_legend_style(TLegend *l1){
@@ -33,63 +33,107 @@ void set_legend_style(TLegend *l1){
 }
 
 int main(){
+	float ht;
+	float w_isr_tr, w_lumi, weight;
 	const int nfiles = 4;
 	TString inputdir, outputdir;
 	outputdir = "/cms/scratch/yjeong/CMSSW_10_2_13/src/RPV/rpv_macros/plots_ht/";
-	inputdir = "/xrootd_user/yjeong/xrootd/nanoprocessing/2016/merged_rpvfitnbge0/";
+	//inputdir = "/xrootd_user/yjeong/xrootd/nanoprocessing/2016/merged_rpvfitnbge0/";
+	inputdir = "/xrootd_user/yjeong/xrootd/nanoprocessing/2016/processed/";
 
 	TString filename[nfiles] = {
-		"600to800_TuneCUETP8M1_18",
-		"800to1200_TuneCUETP8M1_14",
-		"1200to2500_TuneCUETP8M1_7",
-		"2500toInf_TuneCUETP8M1_4"
+		"A73A4F60-87D6-0B44-A377-1B29B24DF617_fatjetbaby_TTJets_HT-2500toInf_TuneCUETP8M1.root",
+		"CEFBBD30-07B3-EB43-8325-D4E887570D33_fatjetbaby_TTJets_HT-1200to2500_TuneCUETP8M1.root",
+		"CCF461B3-DC23-2549-968C-6F28A8A72D77_fatjetbaby_TTJets_HT-800to1200_TuneCUETP8M1.root",
+		"8C973745-0EDF-484F-A51D-950BA631194E_fatjetbaby_TTJets_HT-600to800_TuneCUETP8M1.root"
 	};
+	//---------------TTJets_HT_All-------------------
+	TString alltt;
+	alltt = "8F42BDAD-0FF8-6849-9BF0-D55E5D02CD6D_fatjetbaby_TTJets_TuneCUETP8M1.root";
+	TFile *f1;
+	TTree *t1;
+	TH1F *h2;
+	TCanvas *c1;
+	c1 = new TCanvas;
+	c1->SetLogy();
+	f1 = new TFile(inputdir+alltt);
+	t1 = (TTree*)f1->Get("tree");
+	h2 = new TH1F("h2","TTJets_HT",25,0,5000);
+	for(int i = 0; i<t1->GetEntries(); i++){
+		t1->GetEntry(i);
+		t1->SetBranchAddress("ht",&ht);
+		t1->SetBranchAddress("w_isr_tr",&w_isr_tr);
+		t1->SetBranchAddress("w_lumi",&w_lumi);
+		t1->SetBranchAddress("weight",&weight);
+		h2->Fill(ht,weight);
+	}
+	h2->Draw("hist");
+	c1->SaveAs(outputdir+"TTJets_All_Ht.png");
+	//------------------------------------------------
 
-	TFile *tfile[nfiles], wfile[nfiles];
+	TFile *tfile[nfiles];
 	TTree *tree[nfiles];
 	TH1F *h1[nfiles];
-	TCanvas *c;
-	c = new TCanvas;
+	TCanvas *c2;
+	c2 = new TCanvas;
+	c2->SetLogy();
+	TCanvas *c3;
+	c3 = new TCanvas;
+	c3->SetLogy();
 	TLegend *l;
+	THStack *hs;
 	l = new TLegend(0.65,0.54,0.75,0.8);
 	set_legend_style(l);
-	float ht;
-	float w_isr_tr, w_lumi, weight;
+	float ymax = 0;
+	hs = new THStack("hs","TTJets_HT");
 	for(int i = 0; i < nfiles; i++){
-		tfile[i] = new TFile(inputdir+"TTJets_HT-"+filename[i]+".root");
+		tfile[i] = new TFile(inputdir+filename[i]);
 		tree[i] = (TTree*)tfile[i]->Get("tree");
 		tree[i]->SetBranchAddress("ht",&ht);
 		tree[i]->SetBranchAddress("w_isr_tr",&w_isr_tr);
 		tree[i]->SetBranchAddress("w_lumi",&w_lumi);
 		tree[i]->SetBranchAddress("weight",&weight);
 
-		h1[i] = new TH1F(Form("h1_%d",i),Form("TTJets_HT"),100,1100,7000);
-
+		h1[i] = new TH1F(Form("h1_%d",i),Form("TTJets_HT"),25,0,5000);
+		set_histo_style(h1[i]);
 		for(int j=0; j<tree[i]->GetEntries();j++){
 			tree[i]->GetEntry(j);
-			h1[i]->Fill(ht,w_lumi);
+			h1[i]->Fill(ht,weight);
 		}
 
 		cout<<"weight: "<<weight<<", w_isr: "<<w_isr_tr<<", w_lumi: "<<w_lumi<<endl;
-		if(i==0)h1[i]->SetLineColor(kRed);
-		else if(i==1)h1[i]->SetLineColor(kBlue);
-		else if(i==2)h1[i]->SetLineColor(kGreen);
-		else if(i==3)h1[i]->SetLineColor(kOrange+1);
-
-		if(i==0)l->AddEntry(h1[i],"600to800");
-		else if(i==1)l->AddEntry(h1[i],"800to1200");
-		else if(i==2)l->AddEntry(h1[i],"1200to2500");
-		else if(i==3)l->AddEntry(h1[i],"2500toInf");
-
-		float ymax = 0;
-		ymax = h1[i]->GetMaximum();
-
 		if(i==0){
-			h1[i]->SetMaximum(ymax*1.5);
+			h1[i]->SetLineColor(kRed);
+			l->AddEntry(h1[i],"600to800");
+		}
+		else if(i==1){
+			h1[i]->SetLineColor(kBlue);
+			l->AddEntry(h1[i],"800to1200");
+		}
+		else if(i==2){
+			h1[i]->SetLineColor(kGreen);
+			l->AddEntry(h1[i],"1200to2500");
+		}
+		else if(i==3){
+			h1[i]->SetLineColor(kOrange+1);
+			l->AddEntry(h1[i],"2500toInf");
+		}
+		hs->Add(h1[i]);
+		if(i==3)ymax = h1[i]->GetMaximum();
+		c2->cd();
+		if(i==0){
+			h1[i]->SetMaximum(ymax*100000);
 			h1[i]->Draw("hist");
 		}
 		else if(i>0)h1[i]->Draw("histsame");
-		l->Draw();
+
 	}
-	c->SaveAs(outputdir+"TTJets_ht_lumi.png");
+	l->Draw();
+	c2->SaveAs(outputdir+"TTJets_Ht.png");
+	c3->cd();
+	hs->SetMaximum(ymax*100000);
+	hs->Draw("hist");
+	h2->Draw("histsame");
+	l->Draw();
+	c3->SaveAs(outputdir+"TTJets_Ht_stack.png");
 }

869aef934d33f0a093a1431ce20877ad4c49406d
Chang Whan Jung <changhwanjung95@gmail.com>
2020-04-06 15:10:20 +0900

    datacard management program added

---
+++ rpv_macros/datacards/combine.py
@@ -0,0 +1,23 @@
+import os
+import sys
+
+mass = sys.argv[1]
+
+if len(sys.argv) == 2:
+	g = open("result.txt","w")
+	dname = 'datacard_M'+str(mass)+'_mconly_'
+	for nbs in {"nb1","nb2","nb3","nb4"}:
+		for njets in {"low","med","high"}:
+			region = "sr"
+			if nbs=="nb1" or njets=="low":
+				region = "cr"
+			fname = dname+region+"_"+nbs+"_"+njets+"njets_nopdfoutput_nominal_newnt.dat"
+			os.system("./combine.sh "+fname)
+			f = open("combine.txt","r")
+			res = f.read()
+			f.close()
+			spl_res = res.split()
+			exp50 = spl_res[spl_res.index("50.0%:")+3]
+			pline = nbs + " / " + njets + "jets : " + exp50 + "\n"
+			g.write(pline)
+	g.close()
---
+++ rpv_macros/datacards/combine.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+export TERM=linux
+
+combine -M AsymptoticLimits $1 > combine.txt

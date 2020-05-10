c7a90993ddf17ec6dd945961b6c40cc3c01aa8c4
yjeong <yonghojeong57@gmail.com>
2020-05-10 22:20:29 +0900

    remove the lep_eff in the two scripts

--- rpv_macros/run_kappa.sh
+++ rpv_macros/run_kappa.sh
@@ -2,7 +2,6 @@
 ./run/plot_kappa.exe jes 500 1400
 ./run/plot_kappa.exe btag_bc 500 1400
 ./run/plot_kappa.exe btag_udsg 500 1400
-./run/plot_kappa.exe lep_eff 500 1400
 ./run/plot_kappa.exe mur 500 1400
 ./run/plot_kappa.exe muf 500 1400
 ./run/plot_kappa.exe murf 500 1400
--- rpv_macros/run_make_variation_loop.sh
+++ rpv_macros/run_make_variation_loop.sh
@@ -2,7 +2,6 @@
 ./run/make_variations_loop_newntuple.exe jes 500 1400
 ./run/make_variations_loop_newntuple.exe btag_bc 500 1400
 ./run/make_variations_loop_newntuple.exe btag_udsg 500 1400
-./run/make_variations_loop_newntuple.exe lep_eff 500 1400
 ./run/make_variations_loop_newntuple.exe mur 500 1400
 ./run/make_variations_loop_newntuple.exe muf 500 1400
 ./run/make_variations_loop_newntuple.exe murf 500 1400

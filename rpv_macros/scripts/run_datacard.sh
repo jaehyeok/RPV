b965c5e7ca2822d00782373d2c940870c2056154
yjeong <yonghojeong57@gmail.com>
2020-06-12 01:21:04 +0900

    edit the script of datacard

--- rpv_macros/scripts/run_datacard.sh
+++ rpv_macros/scripts/run_datacard.sh
@@ -1,4 +1,4 @@
-for mass in {1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000}
+for mass in {1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200}
 do echo ${mass}
 ./run/make_rpv_datacard_newbins.exe ${mass} mconly output_newnt.root
 done
@@ -7,12 +7,12 @@ mkdir -p datacards/nopdfoutput_newnt/
 
 cp datacards/*mconly_nopdfoutput_newnt.dat datacards/nopdfoutput_newnt/.
 
-for mass in {1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000}
+for mass in {1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200}
 do echo ${mass}
 mv datacards/nopdfoutput_newnt/datacard_M${mass}_mconly_nopdfoutput_newnt.dat datacards/nopdfoutput_newnt/datacard_M${mass}.dat
 done
 
-for mass in {1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000}
+for mass in {1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200}
 do echo ${mass}
 combine -M AsymptoticLimits datacards/nopdfoutput_newnt/datacard_M${mass}.dat -n M${mass}
 done

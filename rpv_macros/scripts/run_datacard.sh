f8151c46357c7db73fac3df5e4cf35bb88bc1265
YongHo Jeong <yongho.jeong@cern.ch>
2021-02-10 15:54:40 +0900

    bug fix in combine script

--- rpv_macros/scripts/run_datacard.sh
+++ rpv_macros/scripts/run_datacard.sh
@@ -1,5 +1,5 @@
 for mass in {1000,1100,1200,1300,1400,1500,1600,1700,1800,1900,2000,2100,2200}
-do echo ${mass}
+do echo "Gluino mass ${mass} GeV working..."
 	if [ $1 = "2016" ]; then
 		./run/make_rpv_datacard_newbins.exe ${mass} mconly output_$1.root $1 off on
 	else

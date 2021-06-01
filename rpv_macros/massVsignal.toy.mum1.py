40430b80123fa68020698f1fb4f3207e89f4f33e
Chang Whan Jung <changhwanjung95@gmail.com>
2021-06-01 19:24:01 +0900

    signal injection study added

---
+++ rpv_macros/massVsignal.toy.mum1.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+import re
+from sys import argv, stdout, stderr, exit
+import datetime
+from optparse import OptionParser
+import HiggsAnalysis.CombinedLimit.calculate_pulls as CP
+import ROOT
+import numpy as np
+
+nDivisions = 510 
+xTitleOffset = 1.4
+yTitleOffset = 1.5
+TitleSize = 0.043
+LabelSize = 0.06
+LegendSize = 0.06
+PadLeftMargin = 0.08
+PadRightMargin = 0.05
+PadTopMargin = 0.09
+PadBottomMargin =0.08
+nFont = 42
+
+TitleSize=TitleSize/2
+LabelSize=LabelSize/2
+LegendSize=LegendSize/2
+
+ROOT.gStyle.SetCanvasDefW(600);
+ROOT.gStyle.SetCanvasDefH(600);
+ROOT.gStyle.SetTextSize(LegendSize)
+ROOT.gStyle.SetTitleFontSize(TitleSize)
+ROOT.gStyle.SetPadLeftMargin(PadLeftMargin)
+ROOT.gStyle.SetPadRightMargin(PadRightMargin)
+ROOT.gStyle.SetPadTopMargin(PadTopMargin)
+ROOT.gStyle.SetPadBottomMargin(PadBottomMargin)
+ROOT.gStyle.SetNdivisions(nDivisions, "xyz")
+ROOT.gStyle.SetTitleFont(nFont,"xyz")
+ROOT.gStyle.SetLabelFont(nFont,"xyz")
+ROOT.gStyle.SetTextFont(nFont)
+
+r_arry = []
+err_arry = []
+r_min_arry = []
+r_max_arry = []
+
+TH1_1800 = ROOT.TH1F("1800 GeV", "1800 GeV",320,-0.1,3.1)
+TH1_1500 = ROOT.TH1F("1500 GeV", "1500 GeV",320,-0.1,3.1)
+for mass in list(range(1500,2300,100)):
+	up      =[]
+	down    =[]
+	central =[]
+	for seed in list(range(1,1000)):
+		if seed%100==0 : print(str(mass)+" GeV, "+str(seed)+"th loop")
+		fitresult = "out_injection/higgsCombine_M"+str(mass)+".FitDiagnostics.mH120."+str(seed)+".root"
+		file = ROOT.TFile(fitresult)
+		if file == None : raise RuntimeError, "Cannot open file %s" % fitresult
+		limit_t = file.Get("limit"); 
+		if limit_t == None or limit_t.ClassName() != "TTree" : raise RuntimeError, "File does not contain TTree output of the signal fit 'limit'"
+		limit_t.GetEntry(3)
+		stdev_r = limit_t.limit
+		limit_t.GetEntry(4)
+		value_r = limit_t.limit
+		if stdev_r==0 : continue
+		central.append((value_r-1)/stdev_r)
+		if mass==1500 : TH1_1500.Fill(value_r)
+		elif mass==1800 : TH1_1800.Fill(value_r)
+	med_cent = np.mean(central) 
+	ms = 0
+	for r_i in central :
+		ms = ms +  ((r_i-med_cent)**2)/len(central)
+	rms = np.sqrt(ms)
+	r_arry.append(med_cent)
+	r_max_arry.append(rms)
+hist_mvsr = ROOT.TH1F("GvsS", "Gluino mass vs Signal Strength", 8, 1450, 2250)
+for i in range(0,8) : 
+	hist_mvsr.SetBinContent(i+1,r_arry[i])
+	hist_mvsr.SetBinError(i+1,r_max_arry[i])
+#err_mvsr = ROOT.TGraphAsymmErrors(hist_mvsr)
+#for i in range(0,8) : 
+#	err_mvsr.SetPointError(i,0,0,r_arry[i]-r_min_arry[i],r_max_arry[i]-r_arry[i])
+
+hist_mvsr.SetTitleOffset(xTitleOffset,"x");
+hist_mvsr.SetTitleOffset(yTitleOffset,"y");
+hist_mvsr.SetTitleSize(TitleSize,"xyz")
+hist_mvsr.SetLabelSize(LabelSize,"xyz")
+c = ROOT.TCanvas()
+c.SetGrid()
+hist_mvsr.SetTitle("");
+hist_mvsr.GetYaxis().SetRangeUser(-2, 2)
+hist_mvsr.GetXaxis().SetRangeUser(1450,2250)
+hist_mvsr.GetXaxis().SetTitle("m_{#tilde{g}}")
+hist_mvsr.GetYaxis().SetTitle("#mu")
+hist_mvsr.SetTitleSize(TitleSize)
+hist_mvsr.SetStats(0)
+hist_mvsr.SetMarkerStyle(42)
+hist_mvsr.SetMarkerSize(0.2)
+hist_mvsr.SetLineColor(ROOT.kRed)
+hist_mvsr.SetMarkerColor(ROOT.kRed)
+hist_mvsr.Draw()
+#err_mvsr.SetMarkerStyle(42)
+#err_mvsr.SetMarkerSize(0.5)
+#err_mvsr.SetMarkerColor(ROOT.kRed)
+#err_mvsr.SetLineColor(ROOT.kRed)
+#err_mvsr.Draw("same E2 P")
+cmslabel = "#font[62]{CMS} #scale[0.8]{#font[52]{Work In Progress}}"
+lumilabel = "137fb^{-1} (13 TeV)"
+label = ROOT.TLatex()
+label.SetNDC(ROOT.kTRUE)
+label.SetTextSize(0.03)
+label.SetTextAlign(11)
+label.DrawLatex(PadLeftMargin,1-PadTopMargin+0.02,cmslabel);
+label.SetTextAlign(31)
+label.DrawLatex(1-PadRightMargin,1-PadTopMargin+0.02,lumilabel);
+#b = ROOT.TBox(PadLeftMargin, (PadTopMargin+PadBottomMargin)/2-0.5,1-PadRightMargin, (PadTopMargin+PadBottomMargin)/2+0.5)
+b = ROOT.TBox(0,-1,9,1)
+b.SetFillStyle(3003)
+b.SetFillColor(ROOT.kBlue)
+b.Draw("same")
+hist_mvsr.Draw("same EP")
+c.Print("mass_vs_r_result.pdf")
+
+c2=ROOT.TCanvas()
+TH1_1500.SetLineColor(ROOT.kBlue)
+TH1_1500.SetStats(0)
+TH1_1800.SetLineColor(ROOT.kRed)
+TH1_1500.Draw("hist")
+TH1_1800.Draw("same hist")
+c2.Print("1800vs1500.pdf")
---
+++ rpv_macros/signal_injection.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/sh
+
+function run_combine(){
+  card=`echo datacards/datacard_M${2}_mconly_nopdfoutput_SpB_M${2}.dat`
+  combine -M FitDiagnostics $card --saveNorm --saveShapes -t 1 -s ${1} --saveToys -n _M${2} --expectSignal=1 --toysFrequentist 
+  fitdiag=`echo fitDiagnostics_M${2}.root`
+  fitdiag_aft=`echo fitDiagnostics_M${2}_s${1}.root`
+  higcomb=`echo  higgsCombine_M${2}.FitDiagnostics.mH120.${1}.root`
+  mv $fitdiag out_injection/$fitdiag_aft
+  mv $higcomb out_injection  
+}
+
+th="15"
+
+./run/make_kappa_CRfit.exe on 500 1400 2016
+./run/make_variations_loop_newntuple.exe nominal off 500 1400 2016
+./run/make_variations_loop_newntuple.exe kappa off 500 1400 2016
+for mass in {1500,1600,1700,1800,1900,2000,2100,2200}; do
+  ./run/rescale_variations.exe mconlySplusB variations/output_nominal_newnt_2016.root 2016 ${mass}
+  hadd -f variations/output_SpB_M${mass}.root variations/output_nominal_newnt_2016_mconlySplusB.root variations/output_kappa_newnt_2016.root
+  ./run/make_rpv_datacard_newbins.exe ${mass} mconly output_SpB_M${mass}.root 2016 
+  for x in {1..1000}; do
+    j_count=`jobs -l|wc|awk '{print $1}'`
+    if [[ $j_count -ge $th ]];then
+      until [[ $j_count -lt $th ]] 
+        do j_count=`jobs -l|wc|awk '{print $1}'`
+	sleep 0.1
+      done
+    fi
+    run_combine ${x} ${mass} & 	
+  done
+done

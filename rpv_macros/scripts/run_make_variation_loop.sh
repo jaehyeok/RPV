bf384644342032f7373169e6af7ec936c8bdee31
yjeong <yjeong@cern.ch>
2021-06-22 13:31:32 +0200

    fixed the script

--- rpv_macros/scripts/run_make_variation_loop.sh
+++ rpv_macros/scripts/run_make_variation_loop.sh
@@ -1,8 +1,8 @@
 mkdir -p log/
 if [ $1 = "2016" ]; then
-	./run/make_kappa_CRfit.exe on 500 1400 $1 > log/log_make_kappa_$1.txt &
+	./run/make_kappa_CRfit.exe on 500 1400 $1
 else 
-	./run/make_kappa_CRfit.exe on 500 1400 20178 >  log/log_make_kappa_20178.txt &
+	./run/make_kappa_CRfit.exe on 500 1400 20178
 fi
 for mode in {nominal,jec,btag_bc,btag_udsg,mur,muf,murf,isr,lep_eff,gs,kappa};
 do echo ${mode}

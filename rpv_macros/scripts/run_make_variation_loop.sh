6be1211e8a1b60e2d24081e6b6c053a729da3398
Chang Whan Jung <changhwanjung95@gmail.com>
2021-06-22 17:22:15 +0900

    make_variation_loop change

--- rpv_macros/scripts/run_make_variation_loop.sh
+++ rpv_macros/scripts/run_make_variation_loop.sh
@@ -1,5 +1,6 @@
+./run/make_kappa_CRfit.exe on 500 1400 $1
 mkdir -p log/
-for mode in {nominal,jec,btag_bc,btag_udsg,mur,muf,murf,isr,lep_eff,gs};
+for mode in {nominal,jec,btag_bc,btag_udsg,mur,muf,murf,isr,lep_eff,gs,kappa};
 do echo ${mode}
 	if [ $1 = "2016" ]; then
 		./run/make_variations_loop_newntuple.exe ${mode} off 500 1400 $1 > log/log_make_variation_${mode}.txt & #2016 or 20178

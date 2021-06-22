b9e1325ebe3f163526f3e9b1a784830b35c250e9
yjeong <yjeong@cern.ch>
2021-06-22 13:45:06 +0200

    fixed

--- rpv_macros/scripts/run_make_variation_loop.sh
+++ rpv_macros/scripts/run_make_variation_loop.sh
@@ -1,9 +1,3 @@
-mkdir -p log/
-if [ $1 = "2016" ]; then
-	./run/make_kappa_CRfit.exe on 500 1400 $1
-else 
-	./run/make_kappa_CRfit.exe on 500 1400 20178
-fi
 for mode in {nominal,jec,btag_bc,btag_udsg,mur,muf,murf,isr,lep_eff,gs,kappa};
 do echo ${mode}
 	if [ $1 = "2016" ]; then

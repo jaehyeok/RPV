eda65bb9a628a3b0fdb79483a46c695868b09f27
YongHo Jeong <yongho.jeong@cern.ch>
2021-07-22 13:12:34 +0900

    include pile up systematic

--- rpv_macros/scripts/run_make_variation_loop.sh
+++ rpv_macros/scripts/run_make_variation_loop.sh
@@ -5,7 +5,7 @@ else
         ./run/make_kappa_CRfit.exe on 500 1400 20178
 fi
 
-for mode in {nominal,jec,btag_bc,btag_udsg,mur,muf,murf,isr,lep_eff,gs,kappa};
+for mode in {nominal,jec,btag_bc,btag_udsg,mur,muf,murf,isr,lep_eff,gs,kappa,jer,pileup};
 do echo ${mode}
 	if [ $1 = "2016" ]; then
 		./run/make_variations_loop_newntuple.exe ${mode} off 500 1400 $1 > log/log_make_variation_${mode}.txt & #2016 or 20178

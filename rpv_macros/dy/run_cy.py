f9b1666b73a6e0d7274439a64cb8f154f36a38af
Byeong Jin Hong <hongbj5653@gmail.com>
2023-04-27 22:40:37 +0900

    codes for MJ distribution in DY region

--- rpv_macros/dy/run_cy.py
+++ rpv_macros/dy/run_cy.py
@@ -1,10 +1,14 @@
 import DY_test
+import DY_mj12
 import sys
 
-#years=['2016','2017','2018','20178']
-years=['2016']
+years=['2016','2017','2018','20178']
+#years=['2016']
 
 for year in years :
-	DY_test.DY(DY_test.totpath(year),3,year)
-	DY_test.DY(DY_test.totpath(year),5,year)
-	DY_test.DY(DY_test.totpath(year),7,year)
+	#DY_test.DY(DY_test.totpath(year),3,year)
+	#DY_test.DY(DY_test.totpath(year),5,year)
+	#DY_test.DY(DY_test.totpath(year),7,year)
+	DY_mj12.DY(DY_mj12.totpath(year),3,year)
+	DY_mj12.DY(DY_mj12.totpath(year),5,year)
+	DY_mj12.DY(DY_mj12.totpath(year),7,year)

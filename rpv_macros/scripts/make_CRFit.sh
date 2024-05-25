c3ad47c4cccbfe903be4461f37a4097e3e4d2ffb
Byeong Jin Hong <hongbj5653@gmail.com>
2024-05-25 22:49:40 +0900

    To obtain kappa through CRFit, the input file is modified to include mjsyst and mckappa, and JER is made symm.

--- rpv_macros/scripts/make_CRFit.sh
+++ rpv_macros/scripts/make_CRFit.sh
@@ -34,28 +34,34 @@ for year in ${years[@]}
 		echo "Using Real Data"
 		if [ $1 = "20178" ]
 			then
-			hadd -f variations/output_CRFit_${year}.root variations/output_nominal_newnt_${year}_20178.root variations/output_kappa_newnt_${year}_20178.root
+			#hadd -f variations/output_CRFit_${year}.root variations/output_nominal_newnt_${year}_20178.root variations/output_kappa_newnt_${year}_20178.root
+			hadd -f variations/output_CRFit_${year}.root variations/output_nominal_newnt_${year}_20178.root variations/output_syst_${year}_20178.root variations/output_${year}_mckappa.root variations/output_${year}_mjsyst.root
+			./run/make_jer_symm.exe ${year} variations/output_CRFit_${year}.root
 		else
-			hadd -f variations/output_CRFit_${year}.root variations/output_nominal_newnt_${year}.root variations/output_kappa_newnt_${year}.root
+			#hadd -f variations/output_CRFit_${year}.root variations/output_nominal_newnt_${year}.root variations/output_kappa_newnt_${year}.root
+			hadd -f variations/output_CRFit_${year}.root variations/output_nominal_newnt_${year}.root variations/output_syst_${year}.root variations/output_${year}_mckappa.root variations/output_${year}_mjsyst.root
+			./run/make_jer_symm.exe 2016 variations/output_CRFit_${year}.root
 		fi
 	fi
 	cp variations/output_CRFit_${year}.root datacards/variations/output_CRFit_${year}.root
 	if [ $1 = "20178" ]
 		then
-		./run/make_rpv_datacard_newbins_CRfit.exe 1900 mconly output_CRFit_${year}.root ${year} on
-		combineCards.py datacards/datacard_M1900_mconly_cr_nb0_*_CRFit_${year}.dat datacards/datacard_M1900_mconly_cr_nb1_*_CRFit_${year}.dat > datacards/datacard_M1900_CRFit_${year}.dat
+		#./run/make_rpv_datacard_newbins_CRfit.exe 1800 mconly output_CRFit_${year}.root ${year} on off
+		./run/make_rpv_datacard_newbins_CRfit.exe 1800 mconly output_CRFit_${year}.root ${year} on on
+		combineCards.py datacards/datacard_M1800_mconly_cr_nb0_*_CRFit_${year}.dat datacards/datacard_M1800_mconly_cr_nb1_*_CRFit_${year}.dat > datacards/datacard_M1800_CRFit_${year}.dat
 	else
-		./run/make_rpv_datacard_newbins_CRfit.exe 1900 mconly output_CRFit_${year}.root ${year} off
-		combineCards.py datacards/datacard_M1900_mconly_cr_nb0_*_CRFit_${year}.dat datacards/datacard_M1900_mconly_cr_nb1_*_CRFit_${year}.dat > datacards/datacard_M1900_CRFit_${year}.dat
-		combine -M FitDiagnostics datacards/datacard_M1900_CRFit_${year}.dat --saveNorm --saveShape --cminDefaultMinimizerType Minuit
+		#./run/make_rpv_datacard_newbins_CRfit.exe 1800 mconly output_CRFit_${year}.root ${year} off off 
+		./run/make_rpv_datacard_newbins_CRfit.exe 1800 mconly output_CRFit_${year}.root ${year} off on
+		combineCards.py datacards/datacard_M1800_mconly_cr_nb0_*_CRFit_${year}.dat datacards/datacard_M1800_mconly_cr_nb1_*_CRFit_${year}.dat > datacards/datacard_M1800_CRFit_${year}.dat
+		combine -M FitDiagnostics datacards/datacard_M1800_CRFit_${year}.dat --saveNorm --saveShape --cminDefaultMinimizerType Minuit
 		root -l -q repack.C\(\"CRFit_${year}\"\)
 	fi
 done
 if [ $1 = "20178" ]
 	then
 	cd datacards
-	combineCards.py datacard_M1900_CRFit_2017.dat datacard_M1900_CRFit_2018.dat > datacard_M1900_CRFit_20178.dat
+	combineCards.py datacard_M1800_CRFit_2017.dat datacard_M1800_CRFit_2018.dat > datacard_M1800_CRFit_20178.dat
 	cd - 
-	combine -M FitDiagnostics datacards/datacard_M1900_CRFit_20178.dat --saveNorm --saveShape --cminDefaultMinimizerType Minuit
+	combine -M FitDiagnostics datacards/datacard_M1800_CRFit_20178.dat --saveNorm --saveShape --cminDefaultMinimizerType Minuit
 	root -l -q repack.C\(\"CRFit_20178\"\)
 fi
